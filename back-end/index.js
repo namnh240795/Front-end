ec4fdce9fa0f4e069d07d2e490fba3d4ce74cbd4
Nguyễn Hoài Nam <namnh240795@gmail.com>
2017-10-14 18:48:23 +0700

    function generator

--- back-end/index.js
+++ back-end/index.js
@@ -1,68 +1,42 @@
-// function* OddGenerator(max_number)  {
-//   let current = 1;
-//   yield 1;
-
-//   while (current <= max_number)
-//     yield current+=2;
+// function* evenGenerator(params){
+//   yield params+=2;
 // }
 
-// const MAX = 100;
-// const oddGenerator = OddGenerator(MAX);
-// let i = 0;
+// function* oddGenerator(params){
+//   yield params++;
+//   yield* evenGenerator(params);
+// }
 
-// console.log(oddGenerator.next().value);
-// console.log(oddGenerator.next().value);
+// const ODD_GENERATOR = oddGenerator(1);
 
-// function* id_generator(max_number){
-//   console.log('max_number', max_number)
-
-//   let index = 0;
-//   function hasNext(){
-//     return index <= max_number;
-//   }
-
-//   while(hasNext()){
-//     yield* hasNext();
-//     yield index++;
+// console.log(ODD_GENERATOR.next());
+// // JSON object
+// const CAR = {
+//   manufatorer: 'Honda',
+//   wheel: true,
+//   engine: false,
+//   engineStart: function(){
+//     this.engine = true;
+//   },
+//   engineStop: function(){
+//     this.engine = false;
+//   },
+//   people: ["Mo", "Nam", "THao"],
+//   seats: parseInt("4"),
+//   setNumberOfSeats: function(number){
+//     this.seats = number;
 //   }
 // }
 
-// const MAX = 0;
-// const ID_GENERATOR = id_generator(MAX);
+// console.log(typeof CAR.seats);
+// let car = JSON.stringify(CAR);
 
-// console.log(ID_GENERATOR.hasNext());
+// console.log(
+//   typeof JSON.stringify(CAR)
+// );
+// console.log(JSON.parse(car));
+// JSON.parse("{}");
 
-function* anotherGenerator(i) {
-  console.log("start anotherGenerator", i);
-  yield i + 1;
-  console.log("anotherGenerator 1", i);
-  yield i + 2;
-  console.log("anotherGenerator 2", i);
-  yield i + 3;
-  console.log("anotherGenerator 3", i);
-}
 
-function* generator(i) {
-  console.log("start generator", i);
-  yield i;
-  console.log('generator 1', i);
-  yield* anotherGenerator(i);
-  console.log("generator 2", i);
-  yield i + 10;
-  console.log("generator 3", i);
-}
 
-var gen = generator(10);
-
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-
-// console.log(gen.next().value); // 10
-// console.log(gen.next().value); // 11
-// console.log(gen.next().value); // 12
-// console.log(gen.next().value); // 13
-// console.log(gen.next().value); // 20
 
